1d0a2b547bfe6bf0f3e8cfbd0af537f65e6ca301
SunTiegang <119601306@qq.com>
2018-03-05 23:57:54 +0800

    update

---
+++ rich/src/main/java/cn/com/rich/dao/Et_AdminMapper.java
@@ -0,0 +1,30 @@
+package cn.com.rich.dao;
+
+import cn.com.rich.entity.Et_Admin;
+import cn.com.rich.entity.Et_AdminExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface Et_AdminMapper {
+    int countByExample(Et_AdminExample example);
+
+    int deleteByExample(Et_AdminExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Et_Admin record);
+
+    int insertSelective(Et_Admin record);
+
+    List<Et_Admin> selectByExample(Et_AdminExample example);
+
+    Et_Admin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Et_Admin record, @Param("example") Et_AdminExample example);
+
+    int updateByExample(@Param("record") Et_Admin record, @Param("example") Et_AdminExample example);
+
+    int updateByPrimaryKeySelective(Et_Admin record);
+
+    int updateByPrimaryKey(Et_Admin record);
+}
---
+++ rich/src/main/java/cn/com/rich/entity/Et_Admin.java
@@ -0,0 +1,85 @@
+package cn.com.rich.entity;
+
+import java.util.Date;
+
+public class Et_Admin {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String nickName;
+
+    private Integer power;
+
+    private String isDelete;
+
+    private Date gmtModify;
+
+    private Date gmtCreate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
+    }
+
+    public String getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(String isDelete) {
+        this.isDelete = isDelete == null ? null : isDelete.trim();
+    }
+
+    public Date getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(Date gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+}
